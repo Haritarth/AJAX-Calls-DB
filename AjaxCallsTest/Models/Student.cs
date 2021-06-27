50d2845e3d80ea87fd10f5a9f917126ec7afe288
Haritarth <haritarth@gmail.com>
2021-06-28 02:43:45 +0530

    First Commit

---
+++ AjaxCallsTest/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AjaxCallsTest.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+
+        [Required(ErrorMessage = "First Name is required.")]
+        
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; }
+    }
+}
